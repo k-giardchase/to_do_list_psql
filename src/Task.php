539f6885715b333d3eef3252099117907476f2fa
Kyle Giard-Chase & Connor Abdelnoor <student@epicodus.com>
2015-03-16 12:23:34 -0700

    Add functionality to task.php to pass tests in TaskTest.php

--- src/Task.php
+++ src/Task.php
@@ -3,10 +3,14 @@
     {
 
         private $description;
+        private $id;
 
-        function __construct($description)
+        function __construct($description, $id)
         {
-            $this->description = $descrition;
+            $this->description = $description;
+            if($id !== null){
+                $this->id = $id;
+            }
         }
 
         function setDescription($new_description)
@@ -19,19 +23,40 @@
             return $this->description;
         }
 
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function setId($new_id)
+        {
+            $this->id = (int) $new_id;
+        }
+
         function save()
         {
-            array_push($_SESSION['list_of_taks']);
+            $statement = $GLOBALS['DB']->query("INSERT INTO tasks (description) VALUES  ('{$this->getDescription()}') RETURNING id;");
+            $result = $statement->fetch(PDO::FETCH_ASSOC);
+            $this->setId($result['id']);
         }
 
         static function getAll()
         {
-            return $_SESSION['list_of_tasks'];
+            $returned_tasks = $GLOBALS['DB']->query("SELECT * FROM tasks;");
+            $tasks = array();
+            foreach($returned_tasks as $task){
+                $description = $task['description'];
+                $id = $task['id'];
+                $new_task = new Task($description, $id);
+                array_push($tasks, $new_task);
+            }
+
+            return $tasks;
         }
 
         static function deleteAll()
         {
-            $_SESSION['list_of_tasks'] = array();
+            $GLOBALS['DB']->exec("DELETE FROM tasks *;");
         }
     }
 
