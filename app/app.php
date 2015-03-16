199328de19b9f0eb5e2807f178ea5682d8b75e30
Kyle Giard-Chase & Connor Abdelnoor <student@epicodus.com>
2015-03-16 14:40:40 -0700

    Create views to display tasks and categories.

--- app/app.php
+++ app/app.php
@@ -2,30 +2,64 @@
 
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Task.php";
+    require_once __DIR__."/../src/Category.php";
 
     $app = new Silex\Application();
+    $app['debug'] = true;
 
     $DB = new PDO('pgsql:host=localhost;dbname=to_do');
 
-    $app->register(new Silex\Provider\TwigServiceProvider() array(
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
             'twig.path' => __DIR__.'/../views'
     ));
 
+// Loading initial pages
+
     $app->get('/', function() use ($app) {
 
-        return $app->['twig']->render('tasks.html.twig'), array('tasks' => Task::getAll()));
+        return $app['twig']->render('index.html.twig');
     });
 
-    $app->post('/tasks', function() use ($app) {
-        $task = new Task($_POST['description']);
+    $app->get("/tasks", function() use ($app) {
+       return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
+    });
+
+    $app->get("/categories", function() use ($app) {
+       return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+    });
+
+
+// Using info from forms
+
+    $app->post('/categories', function() use($app){
+        $category = new Category($_POST['name']);
+        $category->save();
+
+        return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+    });
+
+    $app->get('/tasks', function() use ($app) {
+
+        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
+    });
+
+    $app->post("/tasks", function() use($app){
+        $task = new Task($_POST['name']);
         $task->save();
-        return $app['twig']->render('create_task.html.twig', array('newtask' => $task));
+
+        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
     });
 
     $app->post("/delete_tasks", function() use ($app) {
         Task::deleteAll();
-        return $app['twig']->render('delete_tasks.html.twig');
+        return $app['twig']->render('index.html.twig');
     });
+
+    $app->post("/delete_categories", function() use ($app) {
+        Category::deleteAll();
+        return $app['twig']->render('index.html.twig');
+    });
+
 
     return $app;
 
